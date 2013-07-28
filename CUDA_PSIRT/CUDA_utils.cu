c4b1525a159fb015c426fcc63c4728e4251e3a86
Leonardo Brito <lbrito@gmail.com>
2013-07-28 20:27:01 -0300

    cleanup

--- CUDA_PSIRT/CUDA_utils.cu
+++ CUDA_PSIRT/CUDA_utils.cu
@@ -7,12 +7,3 @@ inline void GPUassert(cudaError_t code, char * file, int line, bool Abort=true)
         if (Abort) exit(code);
     }       
 }
-
-
-__device__ PSIRT* build_psirt_from_parameters(PSIRT* dev_psirt, Trajectory* t, Particle* p, int n_proj, int n_traj, int n_part);
-
-
-__device__ PSIRT* build_psirt_from_parameters(PSIRT* dev_psirt, Trajectory* t, Particle* p, int n_proj, int n_traj, int n_part)
-{
-	
-}
--- CUDA_PSIRT/kernel.cu
+++ CUDA_PSIRT/kernel.cu
@@ -5,15 +5,12 @@
 #include <stdio.h>
 #include <windows.h>
 #include <time.h>
-#include <GL/glut.h>
 
-#include <cuda_gl_interop.h>
 
 #define GPUerrchk(ans) { GPUassert((ans), __FILE__, __LINE__); }
 
 
 //gambiarra
-//#define DEBUG_OPENGL
 //#define DEBUG_PRINT
 
 void prep_psirt();
@@ -22,126 +19,12 @@ void test_psirt();
 
 __global__ void psirt_kernel(PSIRT* psirt, Projection** dev_proj);
 
-GLuint positionsVBO;
-struct cudaGraphicsResource *positionsVBO_CUDA;
-
 PSIRT *debug_host_psirt;
 PSIRT *dev_psirt;
 
 Projection**	dev_projections;
 Particle**		dev_particles;
 
-// ---------------------------
-// *** OPENGL ***
-// Desenhar configuracao (trajetorias)
-// ---------------------------
-void opengl_draw_configuration_lines(PSIRT* psirt)
-{
- glClear(GL_COLOR_BUFFER_BIT);
-
-
-
-  glBegin(GL_LINES);
-  int i, j;
-	for (i = 0; i < psirt->n_projections; i++)
-	{
-		for (j = 0; j < psirt->n_trajectories; j++)
-		{
-			Trajectory* t = &(psirt->trajectories[(i*psirt->n_projections)+j]);
-
-			Vector2D begin, end, d;
-			sum_void(&(t->source), &(t->direction), &begin);
-			d.x = t->direction.x;
-			d.y = t->direction.y;
-	
-			mult_constant_void(&d, -1);
-			sum_void(&(t->source), &d, &end);
-
-			glColor3f(1.0, 1.0, 1.0);
-			glVertex2f(begin.x, begin.y);
-			glVertex2f(end.x, end.y);
-			
-		}
-	}
-	glEnd();
-}
-
-// ---------------------------
-// *** OPENGL ***
-// Desenhar particulas
-// ---------------------------
-void opengl_draw_particles(PSIRT* psirt)
-{
-	int i=0;
-	glPointSize(7.0);
-	glBegin(GL_POINTS);
-	for (i = 0; i < psirt->n_particles; i++) {
-		if (psirt->particles[i].status != DEAD) {
-			glColor3f(1.0, 0.0, 0.0);
-			glVertex2f(psirt->particles[i].location.x, psirt->particles[i].location.y);
-		} else {
-			glColor3f(0.0, 1.0, 0.0);
-			glVertex2f(psirt->particles[i].location.x, psirt->particles[i].location.y);
-		}
-		//printf("\r\n%d: \t%f,%f",i,psirt->particles[i]->location->x, psirt->particles[i]->location->y);
-	}
-
-	glEnd();
-	glFlush();  // Render now
-}
-
-void opengl_draw()
-{
-	
-
-	// Copia dev->host
-	cudaMemcpy(debug_host_psirt, dev_psirt, sizeof(PSIRT), cudaMemcpyDeviceToHost);
-	//printf("\r\nAAA->%d",debug_host_psirt->optim_max_iterations);
-	opengl_draw_configuration_lines(debug_host_psirt);
-	opengl_draw_particles(debug_host_psirt);
-
-	
-}
-void update()
-{
-	// Executa uma iteração
-//	psirt_kernel<<<1,1>>>(dev_psirt);
-	
-	// Desenha
-	glutPostRedisplay();
-}
-
-void keyboard_handler (unsigned char key, int x, int y)
-{
-	if (key == 27) exit(0);	//ESC = exit
-}
-
-
-void init_opengl(int argc, char* argv[])
-{
-	glutInit(&argc, argv);
-	glutCreateWindow("PSIRT_CUDA");
-	glutReshapeWindow(RES_X, RES_Y);
-	glutDisplayFunc(opengl_draw);
-	glutIdleFunc(update);
-	glutKeyboardFunc(keyboard_handler);
-	glutMainLoop();
-}
-
-
-
-
-
-
-
-
-
-
-
-__global__ void ts(Vector2D* l)
-{
-	printf("\r\nx=%f",l[0].x);
-}
 
 __global__ void test(Trajectory* t, Particle* p, int* dev_params, PSIRT* dev_psirt)
 {
@@ -200,9 +83,7 @@ __global__ void test(Trajectory* t, Particle* p, int* dev_params, PSIRT* dev_psi
 		{
 			if (dev_psirt->optim_curr_part < dev_psirt->n_particles) optimize(dev_psirt);						// otimizacao (comecar)
 			else done = 1;	// DONE 
-		//	done = 1;
 		}
-//	return 0;	// NOT DONE
 	}
 }
 
@@ -263,64 +144,20 @@ void test_psirt(PSIRT* host_psirt)
 
 	// 4. COPIAR DE VOLTA
 	Particle *host_plist = host_psirt->particles;
-	
-		
 	GPUerrchk(cudaMemcpy( host_plist, part, sizeof(Particle) * n_part, cudaMemcpyDeviceToHost));
-	
-
 }
-
-
-
-
-__global__ void psirt_kernel(PSIRT* psirt, Projection** dev_proj)
-{
-	//int q = 0;
-//	printf("%d",q);
-//	for(q=0;q<100;q++){
-	//	printf("\r\n\r\n%d",q);
-	// ---------------------------
-	// *** ATUALIZAR POSICOES DAS PARTICULAS ***
-	// ---------------------------
-	// ---------------- update_particles(psirt);
-
-	int i=0,j=0,k=0;
-	Vector2D resultant_force;
-	//printf("\r\nProj = %d",dev_proj[0]->n_traj);
-	//printf("\r\nProj = %d",dev_projections[0]->n_traj);
-	//psirt->n_particles = 2; // funciona
-}
-
-
 
 int main(int argc, char* argv[])
 {
 	cudaError_t cudaStatus;
 
-		PSIRT* host_psirt = init_psirt();
-	
-	#ifdef DEBUG_OPENGL
-		// 1. INICIALIZAR (HOST)
-		debug_host_psirt = init_psirt();
+	PSIRT* host_psirt = init_psirt();	
+	cudaError_t cuda_status = cudaSetDevice(0);
+	if (cuda_status != cudaSuccess) printf("\r\n cuInit failed");
 
-		// 2. COPIAR (HOST->DEV)
-		cudaMalloc((void**)&dev_psirt, sizeof(PSIRT));
-		cudaMemcpy(dev_psirt, debug_host_psirt, sizeof(PSIRT), cudaMemcpyHostToDevice);
+	//prep_psirt(); // CUDA
 
-		// 3. EXECUTAR / DESENHAR
-		init_opengl(argc, argv);
-	#endif
-
-	#ifndef DEBUG_OPENGL
-		cudaError_t cuda_status = cudaSetDevice(0);
-		if (cuda_status != cudaSuccess) printf("\r\n cuInit failed");
-
-		//prep_psirt(); // CUDA
-
-		test_psirt(host_psirt);
-	#endif
-
-	
+	test_psirt(host_psirt);
 
     
 	//printf("\r\nOK!");
