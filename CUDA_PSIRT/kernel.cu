3e9b1f965a481fbf7c123b292eb277590e1848d7
Leonardo Brito <lbrito@gmail.com>
2013-07-28 22:00:32 -0300

    PSIRT cuda com otimizacao OK

--- CUDA_PSIRT/kernel.cu
+++ CUDA_PSIRT/kernel.cu
@@ -24,17 +24,15 @@ __global__ void run_cuda_psirt(Trajectory* t, Particle* p, int* dev_params, PSIR
 	dev_psirt->n_trajectories = dev_params[1];
 	dev_psirt->n_particles = dev_params[2];
 
-	dev_psirt->is_optimized = 0;
-	dev_psirt->is_optimizing_dirty_particle = 0;
-	dev_psirt->optim_is_ranked = 0;
-	dev_psirt->optim_curr_part = 0;
-	dev_psirt->optim_curr_iteration = 0;
-	dev_psirt->optim_max_iterations = 100;
+	int is_optimized = 0;
+	int is_optimizing_dirty_particle = 0;
+	int optim_is_ranked = 0;
+	int optim_curr_part = 0;
+	int optim_curr_iteration = 0;
+	int optim_max_iterations = 100;
 
 	int npart = dev_psirt->n_particles;
 	int ttl_trajs = dev_psirt->n_trajectories * dev_psirt->n_projections;
-
-	int is_optimizing_dirty_particle = 0;
 
 	printf("\r\n===========\r\nSTARTUP CUDA PSIRT\r\n===========\r\nPARAMS:");
 	printf("\t(#PROJ)\t(#TRAJ)\t(NPART)\r\n\t%d\t%d\t%d\r\n\r\n",dev_psirt->n_projections, dev_psirt->n_trajectories, dev_psirt->n_particles);
@@ -42,7 +40,7 @@ __global__ void run_cuda_psirt(Trajectory* t, Particle* p, int* dev_params, PSIR
 	int done = 0;
 	int lim = 0;
 
-	while (!done&(++lim<1000))
+	while (!done&(++lim<2000))
 	{
 			// ---------------------------
 		// *** ATUALIZAR POSICOES DAS PARTICULAS ***
@@ -59,7 +57,6 @@ __global__ void run_cuda_psirt(Trajectory* t, Particle* p, int* dev_params, PSIR
 			t[i].n_particulas_atual = 0;
 			for (j=0; j<npart; j++)
 			{
-				
 				if (p[j].status == ALIVE)
 				{
 					float distance_point_line = distance(&p[j].location,&t[i]);
@@ -75,16 +72,90 @@ __global__ void run_cuda_psirt(Trajectory* t, Particle* p, int* dev_params, PSIR
 		// ---------------------------
 		// *** OTIMIZACAO E CONVERGENCIA ***
 		// ---------------------------
-		optimization_check(dev_psirt);					// otimizacao (continuar)
+		// pre-update otimizacao
+		if (is_optimizing_dirty_particle) {
+			// OTIMIZANDO
+			if (optim_curr_iteration < optim_max_iterations) {
+				optim_curr_iteration++;
+			}
+			// OTIMIZACAO FALHOU (EXCEDEU MAX ITERACOES)
+			else {
+				//			printf("\r\n[OPTIM]\tPARTICLE #%d LIVED (ITER #%d)",optim_curr_part,optim_curr_iteration);
+				p[optim_curr_part].status = ALIVE; // NAO CONSEGUIU REMOVER
+				optim_curr_part++;
+				is_optimizing_dirty_particle = 0;
+			}
+		}
 
 		int stable = 0;
 		for (i=0;i<ttl_trajs;i++) 
 		{
 			if (t[i].n_particulas_atual>=t[i].n_particulas_estavel)	stable ++;
 		}
-		if (stable==ttl_trajs) // is stable
+		if (stable==ttl_trajs) // is stable					*************(trecho ok)
 		{
-			if (dev_psirt->optim_curr_part < dev_psirt->n_particles) optimize(dev_psirt);						// otimizacao (comecar)
+			if (optim_curr_part < npart) 
+			{
+				// optimize
+				// ---------------------------
+				// *** PRIMEIRO PASSO ***
+				// Deve-se ordenar as particulas de acordo
+				// com a quantidade de trajetorias a que
+				// cada uma atende (0 a 3), ordem crescente.
+				// ---------------------------
+				if (!optim_is_ranked)
+				{
+					Particle temp;
+					for (i = 0; i < npart; i++)
+					{
+						for (j = npart-1; j > i; j--)
+						{
+							if ((p[j].current_trajectories < p[i].current_trajectories) & (i!=j))  
+							{
+								temp = p[i];
+								p[i] = p[j];
+								p[j] = temp;
+							}
+						}
+					}
+					optim_is_ranked = 1;
+				}
+
+				// ---------------------------
+				// CASO ESPECIAL
+				// partícula sem trajetoria, ELIMINAR SEM CHECAR
+				// ---------------------------
+				if (p[optim_curr_part].current_trajectories == 0)
+				{
+					is_optimizing_dirty_particle = 0;
+					p[optim_curr_part].status = DEAD;
+					optim_curr_part++;
+				}
+
+				// ---------------------------
+				// CASO NORMAL
+				// partícula atende a mais de 0 trajetórias, CHECAR ANTES
+				// ---------------------------
+				else
+				{
+					is_optimizing_dirty_particle = 1;
+
+					// COMECAR A CHECAR PARTICULA
+					if (p[optim_curr_part].status == ALIVE)
+					{
+						optim_curr_iteration = 0;
+						p[optim_curr_part].status = CHECKING;
+					}
+					// PARTICULA CHECADA & CONVERGIU -> REMOVER
+					else if (p[optim_curr_part].status == CHECKING)
+					{
+						p[optim_curr_part].status = DEAD;
+						optim_curr_part++;
+					}
+				}
+			}
+			// (end optim)
+
 			else done = 1;	// DONE 
 		}
 	}
