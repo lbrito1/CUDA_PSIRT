1212e554a0e2560652fa76336fc8306ebd939b08
Leonardo Brito <lbrito@gmail.com>
2013-07-30 16:59:10 -0300

    paralelizando

--- CUDA_PSIRT/kernel.cu
+++ CUDA_PSIRT/kernel.cu
@@ -17,7 +17,8 @@ void cuda_psirt(PSIRT* host_psirt);
 
 __global__ void run_cuda_psirt(Trajectory* t, Particle* p, int* dev_params, PSIRT* dev_psirt)
 {
-	 int index = blockIdx.x * blockDim.x + threadIdx.x;
+	// Indice da partícula a ser tratada nesta thread
+	int part_index = blockIdx.x * blockDim.x + threadIdx.x;
 
 	dev_psirt->particles = p;
 	dev_psirt->trajectories = t;
@@ -49,16 +50,195 @@ __global__ void run_cuda_psirt(Trajectory* t, Particle* p, int* dev_params, PSIR
 		// *** ATUALIZAR POSICOES DAS PARTICULAS ***
 		// ---------------------------
 		int i=0,j=0;
-		Vector2D resultant_force;
+		Vector2D resultant_force, resultant_vector;
+		//for (i = 0; i < dev_psirt->n_particles; i++) 
+		//{
+		if (p[part_index].status != DEAD) 
+		{		
+			set(&resultant_force,0.0,0.0);
+			set(&resultant_vector,0.0,0.0);
+			for (j = 0; j < ttl_trajs; j++) 
+			{
+				resultant(&(t[j]),&p[part_index], &resultant_vector);
+				sum_void(&resultant_force, &resultant_vector, &resultant_force);
+			}
+			set(&resultant_force, -resultant_force.x, -resultant_force.y);
+			update_particle(&p[part_index], &resultant_force);
+		}
+		//}																// !!!!!!!!!!!!!!!!!!!!! paralelizar
+		__syncthreads();
+		// ---------------------------
+		// *** CALCULO DE TRAJETORIAS SATISFEITAS ***
+		// ---------------------------
+		for (i=0;i<dev_psirt->n_particles;i++) dev_psirt->particles[i].current_trajectories = 0; 	// zera #traj de cada particula
+		for (i=0;i<ttl_trajs; i++) 
+		{
+			t[i].n_particulas_atual = 0;
+			for (j=0; j<npart; j++)																	// !!!!!!!!!!!!!!!!!!!!! paralelizar
+			{
+				if (p[j].status == ALIVE)
+				{
+					float distance_point_line = distance(&p[j].location,&t[i]);
+					if (distance_point_line<TRAJ_PART_THRESHOLD)
+					{
+						t[i].n_particulas_atual++;
+						p[j].current_trajectories++;
+					}
+				}
+				
+			}
+		}
+		__syncthreads();
+		// ---------------------------
+		// *** OTIMIZACAO E CONVERGENCIA ***
+		// ---------------------------
+		// pre-update otimizacao
+		if (is_optimizing_dirty_particle) {
+			// OTIMIZANDO
+			if (optim_curr_iteration < optim_max_iterations) {
+				optim_curr_iteration++;
+			}
+			// OTIMIZACAO FALHOU (EXCEDEU MAX ITERACOES)
+			else {
+				//			printf("\r\n[OPTIM]\tPARTICLE #%d LIVED (ITER #%d)",optim_curr_part,optim_curr_iteration);
+				p[optim_curr_part].status = ALIVE; // NAO CONSEGUIU REMOVER
+				optim_curr_part++;
+				is_optimizing_dirty_particle = 0;
+			}
+		}
+		__syncthreads();
+		int stable = 0;
+		for (i=0;i<ttl_trajs;i++) 
+		{
+			if (t[i].n_particulas_atual>=t[i].n_particulas_estavel)	stable ++;
+		}
+
+		if (lim>5000) {
+			
+		}
+
+		if (stable==ttl_trajs) // is stable					*************(trecho ok)
+		{
+			// no optim
+			done = 1;
+
+
+			//if (optim_curr_part < npart) 
+			//{
+				// optimize
+				// ---------------------------
+				// *** PRIMEIRO PASSO ***
+				// Deve-se ordenar as particulas de acordo
+				// com a quantidade de trajetorias a que
+				// cada uma atende (0 a 3), ordem crescente.
+				// ---------------------------
+				/*if (!optim_is_ranked)
+				{
+					Particle temp;
+					for (i = 0; i < npart; i++)
+					{
+						for (j = npart-1; j > i; j--)
+						{
+							if ((p[j].current_trajectories < p[i].current_trajectories) & (i!=j))  
+							{
+								temp = p[i];
+								p[i] = p[j];
+								p[j] = temp;
+							}
+						}
+					}
+					optim_is_ranked = 1;
+				}*/
+
+				// ---------------------------
+				// CASO ESPECIAL
+				// partícula sem trajetoria, ELIMINAR SEM CHECAR
+				// ---------------------------
+				//__syncthreads();
+			/*	if (p[part_index].current_trajectories == 0)
+				{
+					is_optimizing_dirty_particle = 0;
+					p[part_index].status = DEAD;
+					optim_curr_part++;
+				}
+
+				// ---------------------------
+				// CASO NORMAL
+				// partícula atende a mais de 0 trajetórias, CHECAR ANTES
+				// ---------------------------
+				else
+				{
+					is_optimizing_dirty_particle = 1;
+
+					// COMECAR A CHECAR PARTICULA
+					if (p[optim_curr_part].status == ALIVE)
+					{
+						optim_curr_iteration = 0;
+						p[optim_curr_part].status = CHECKING;
+					}
+					// PARTICULA CHECADA & CONVERGIU -> REMOVER
+					else if (p[optim_curr_part].status == CHECKING)
+					{
+						p[optim_curr_part].status = DEAD;
+						optim_curr_part++;
+					}
+				}*/
+			//}
+			// (end optim)
+
+			//else done = 1;	// DONE 
+		}
+	}
+}
+
+
+
+
+
+
+
+
+__global__ void run_cuda_psirt_singlethread(Trajectory* t, Particle* p, int* dev_params, PSIRT* dev_psirt)
+{
+	dev_psirt->particles = p;
+	dev_psirt->trajectories = t;
+	
+	dev_psirt->n_projections = dev_params[0];
+	dev_psirt->n_trajectories = dev_params[1];
+	dev_psirt->n_particles = dev_params[2];
+
+	int is_optimized = 0;
+	int is_optimizing_dirty_particle = 0;
+	int optim_is_ranked = 0;
+	int optim_curr_part = 0;
+	int optim_curr_iteration = 0;
+	int optim_max_iterations = 100;
+
+	int npart = dev_psirt->n_particles;
+	int ttl_trajs = dev_psirt->n_trajectories * dev_psirt->n_projections;
+
+	printf("\r\n===========\r\nSTARTUP CUDA PSIRT\r\n===========\r\nPARAMS:");
+	printf("\t(#PROJ)\t(#TRAJ)\t(NPART)\r\n\t%d\t%d\t%d\r\n\r\n",dev_psirt->n_projections, dev_psirt->n_trajectories, dev_psirt->n_particles);
+
+	int done = 0;
+	int lim = 0;
+
+	while (!done)
+	{
+		++lim;
+			// ---------------------------
+		// *** ATUALIZAR POSICOES DAS PARTICULAS ***
+		// ---------------------------
+		int i=0,j=0;
+		Vector2D resultant_force, resultant_vector;
 		for (i = 0; i < dev_psirt->n_particles; i++) 
 		{
 			if (p[i].status != DEAD) 
 			{		
 				set(&resultant_force,0.0,0.0);
-				// calcular força resultante primeiro
-				Vector2D resultant_vector;
 				set(&resultant_vector,0.0,0.0);
-				for (j = 0; j < ttl_trajs; j++) {
+				for (j = 0; j < ttl_trajs; j++) 
+				{
 					resultant(&(t[j]),&p[i], &resultant_vector);
 					sum_void(&resultant_force, &resultant_vector, &resultant_force);
 				}
@@ -119,8 +299,12 @@ __global__ void run_cuda_psirt(Trajectory* t, Particle* p, int* dev_params, PSIR
 
 		if (stable==ttl_trajs) // is stable					*************(trecho ok)
 		{
-			if (optim_curr_part < npart) 
-			{
+			// no optim
+			done = 1;
+
+
+			//if (optim_curr_part < npart) 
+			//{
 				// optimize
 				// ---------------------------
 				// *** PRIMEIRO PASSO ***
@@ -128,7 +312,7 @@ __global__ void run_cuda_psirt(Trajectory* t, Particle* p, int* dev_params, PSIR
 				// com a quantidade de trajetorias a que
 				// cada uma atende (0 a 3), ordem crescente.
 				// ---------------------------
-				if (!optim_is_ranked)
+				/*if (!optim_is_ranked)
 				{
 					Particle temp;
 					for (i = 0; i < npart; i++)
@@ -144,17 +328,17 @@ __global__ void run_cuda_psirt(Trajectory* t, Particle* p, int* dev_params, PSIR
 						}
 					}
 					optim_is_ranked = 1;
-				}
+				}*/
 
 				// ---------------------------
 				// CASO ESPECIAL
 				// partícula sem trajetoria, ELIMINAR SEM CHECAR
 				// ---------------------------
-				__syncthreads();
-				if (p[optim_curr_part].current_trajectories == 0)
+				//__syncthreads();
+			/*	if (p[part_index].current_trajectories == 0)
 				{
 					is_optimizing_dirty_particle = 0;
-					p[optim_curr_part].status = DEAD;
+					p[part_index].status = DEAD;
 					optim_curr_part++;
 				}
 
@@ -178,14 +362,22 @@ __global__ void run_cuda_psirt(Trajectory* t, Particle* p, int* dev_params, PSIR
 						p[optim_curr_part].status = DEAD;
 						optim_curr_part++;
 					}
-				}
-			}
+				}*/
+			//}
 			// (end optim)
 
-			else done = 1;	// DONE 
+			//else done = 1;	// DONE 
 		}
 	}
 }
+
+
+
+
+
+
+
+
 
 void cuda_psirt(PSIRT* host_psirt)
 {
@@ -220,8 +412,40 @@ void cuda_psirt(PSIRT* host_psirt)
 	int n_threads_per_block = 32;
 	int n_blocks = n_elements/n_threads_per_block;
 
+	cudaEvent_t start, start_paralel, stop_1, stop_paralel;
+	cudaEventCreate(&start);
+	cudaEventCreate(&start_paralel);
+	cudaEventCreate(&stop_1);
+	cudaEventCreate(&stop_paralel);
+	
+	cudaEventRecord(start);
+	run_cuda_psirt_singlethread<<<1,1>>>(traj, part, dev_params, dev_psirt);
+	cudaEventRecord(stop_1);
+	
+	
+
+	// zerar
+	cudaDeviceSynchronize();
+	GPUerrchk(cudaMemcpy(traj, host_psirt->trajectories, sizeof(Trajectory) * n_ttl_traj, cudaMemcpyHostToDevice));
+	GPUerrchk(cudaMemcpy(part, host_psirt->particles, sizeof(Particle) * n_part, cudaMemcpyHostToDevice));
+
+	cudaEventRecord(start_paralel);
 	run_cuda_psirt<<<n_blocks,n_threads_per_block>>>(traj, part, dev_params, dev_psirt);
-	//run_cuda_psirt<<<1,1>>>(traj, part, dev_params, dev_psirt);
+	cudaEventSynchronize(stop_paralel);
+
+	cudaDeviceSynchronize();
+
+
+	float ms_1 = 0, ms_par = 0;
+	cudaEventElapsedTime(&ms_1, start, stop_1);
+	cudaEventElapsedTime(&ms_1, start_paralel, stop_paralel);
+	cudaEventDestroy(start);
+	cudaEventDestroy(start_paralel);
+	cudaEventDestroy(stop_1);
+	cudaEventDestroy(stop_paralel);
+
+	printf ("\r\nFINALIZOU EXEC CUDA (1x1)\r\n TEMPO DE EXECUÇÃO FINAL: %f ms\r\n==============\r\n", ms_1);
+	printf ("\r\nFINALIZOU EXEC CUDA (%dx%d)\r\n TEMPO DE EXECUÇÃO FINAL: %f ms\r\n==============\r\n", n_blocks, n_threads_per_block, ms_par);
 
 	// 4. COPIAR DE VOLTA
 	Particle *host_plist = host_psirt->particles;
@@ -246,5 +470,8 @@ int main(int argc, char* argv[])
 	// Limpeza & finalização
 	free(host_psirt);
 	GPUerrchk(cudaDeviceReset());
+
+	getchar();
+
     return 0;
 }
